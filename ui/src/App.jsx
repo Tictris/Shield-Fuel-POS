423d59a30f5b268ad84311e3db3102ccc5d19439
Yousef <picturenow@hotmail.com>
2023-11-13 17:26:22 +0800

    new commit

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,22 +1,9 @@
 import './App.css'
-import Login from './components/pages/Login'
-import Dashboard from './components/pages/superadmin/Dashboard'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UnAuth from './api/UnAuth'
-import Home from './components/layouts/Home'
-import Auth from './api/Auth'
-import Layout from './components/layouts/Layout'
-import Users from './components/pages/superadmin/Users'
-import PageComponents from './components/layouts/PageComponents'
-
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Layout/>
-    </>
+    <></>
   )
 }
 
