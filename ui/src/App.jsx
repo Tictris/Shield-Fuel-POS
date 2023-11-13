399154de53efa6141bed07e7fd0f0224f1a1ac9c
Yousef <picturenow@hotmail.com>
2023-11-13 17:04:27 +0800

    empty login file in ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-<<<<<<< HEAD
 import Login from './components/pages/Login'
 import Dashboard from './components/pages/superadmin/Dashboard'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -9,11 +8,7 @@ import Auth from './api/Auth'
 import Layout from './components/layouts/Layout'
 import Users from './components/pages/superadmin/Users'
 import PageComponents from './components/layouts/PageComponents'
-=======
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
->>>>>>> origin/jason
 
 function App() {
   const [count, setCount] = useState(0)
